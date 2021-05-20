b88b6368478f902695d6f0df67536485a581a9bc
Amerlander <Amerlander@users.noreply.github.com>
2021-05-20 17:26:47 +0200

    Knopf B = Smiley

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Heart)
 })
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Happy)
+})
 basic.forever(function () {
 	
 })
