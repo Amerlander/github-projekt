db4311c1278e4ad34dd123dd15f5b2b8df1572cc
Amerlander <Amerlander@users.noreply.github.com>
2021-05-20 17:28:18 +0200

    Haken statt Herz, Zeige Zahl 0

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.Yes)
 })
 basic.forever(function () {
-	
+    basic.showNumber(0)
 })
