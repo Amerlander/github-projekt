3b5e35eb295d50397b028df2a118e3e3d582c4c5
Amerlander <Amerlander@users.noreply.github.com>
2021-05-20 17:27:18 +0200

    X statt Smiley

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Heart)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Happy)
+    basic.showIcon(IconNames.No)
 })
 basic.forever(function () {
 	
